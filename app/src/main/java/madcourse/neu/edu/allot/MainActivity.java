f7a5d62ad5dde8d38c6f4a887b1dea7a82f8d752
PrachiMobileApp <prachi.mobileapp@gmail.com>
2018-10-09 18:09:48 -0700

    Mobile Application Project
    Academic project for Mobile application development course

---
+++ app/src/main/java/madcourse/neu/edu/allot/MainActivity.java
@@ -0,0 +1,20 @@
+package madcourse.neu.edu.allot;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
